c673375e178a7391211d67acfba563922cf4c1b6
Natalia Shakh <natalia.shakh@valamis.com>
2021-11-07 13:58:37 +0300

    Test3

--- src/MainClass.kt
+++ src/MainClass.kt
@@ -2,14 +2,21 @@ open class MainClass {
 
     companion object {
 
-        const val classNumber: Int = 20
-
-        fun getLocalNumber(): Int {
-            return 14
-        }
-
-        fun getClassNumber(): Int {
-            return classNumber
-        }
+        private val classNumber: Int = 20
+        private val classString: String = "Hello, world"
     }
+
+    fun getLocalNumber(): Int {
+        return 14
+    }
+
+    fun getClassNumber(): Int {
+        return classNumber
+    }
+
+    fun getClassString(): String {
+        return classString
+    }
+
 }
+
--- src/MainClassTest.kt
+++ src/MainClassTest.kt
@@ -1,7 +1,9 @@
+import org.hamcrest.CoreMatchers.containsString
+import org.hamcrest.CoreMatchers.either
 import org.junit.Assert
 import org.junit.Test
 
-class MainClassTest: MainClass() {
+class MainClassTest : MainClass() {
 
     @Test
     fun testGetLocalNumber() {
@@ -9,13 +11,21 @@ class MainClassTest: MainClass() {
         val actual = getLocalNumber()
 
 
-        Assert.assertTrue("The number isn't 14", actual==expected)
+        Assert.assertTrue("The number isn't 14", actual == expected)
     }
 
     @Test
     fun testGetClassNumber() {
         val actual = getClassNumber()
 
-        Assert.assertTrue("The number is less than 45", actual>45)
+        Assert.assertTrue("The number is less than 45", actual > 45)
+    }
+
+    @Test
+    fun testGetClassString() {
+        val actual = getClassString()
+
+        Assert.assertThat(actual, either(containsString("hello")).or(containsString("Hello")))
+        //doesn't need a message
     }
 }
