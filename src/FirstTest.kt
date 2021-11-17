373daf3c2fa4d142851b3eaacf62858088b36d5e
Natalia Shakh <natalia.shakh@valamis.com>
2021-11-17 23:18:51 +0300

    lesson 3

--- src/FirstTest.kt
+++ src/FirstTest.kt
@@ -1,11 +1,15 @@
+
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
+import org.openqa.selenium.By
 import org.openqa.selenium.WebElement
 import org.openqa.selenium.remote.DesiredCapabilities
+import org.openqa.selenium.support.ui.ExpectedConditions
+import org.openqa.selenium.support.ui.WebDriverWait
 import java.net.URL
 
 open class FirstTest {
@@ -36,7 +40,16 @@ open class FirstTest {
         var search: WebElement = driver!!.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]")
         search.click()
 
-        var enter: WebElement = driver!!.findElementById("search_src_text")
+        var enter: WebElement = waitElementById("search_src_text", "Can't find search input", 5)
         enter.sendKeys("Madonna")
+    }
+
+    fun waitElementById(id: String, error: String, time: Long): WebElement {
+        var wait = WebDriverWait(driver, time)
+        wait.withMessage(error + "\n")
+        var by: By = By.id(id)
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        )
     }
 }
