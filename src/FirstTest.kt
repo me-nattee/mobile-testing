49f2bce00fd42efa1d42e281375bf0668f130f08
Natalia Shakh <natalia.shakh@valamis.com>
2021-11-20 21:54:55 +0300

    lesson 3

--- src/FirstTest.kt
+++ src/FirstTest.kt
@@ -1,4 +1,3 @@
-
 import io.appium.java_client.AppiumDriver
 import io.appium.java_client.MobileElement
 import io.appium.java_client.android.AndroidDriver
@@ -37,11 +36,17 @@ open class FirstTest {
 
     @Test
     fun firstTest() {
-        var search: WebElement = driver!!.findElementByXPath("//*[contains(@text, 'Search Wikipedia')]")
-        search.click()
+        clickByXpath("//*[contains(@text, 'Search Wikipedia')]", "Can't find Search Wikipedia input", 5)
+        sendKeysById("search_src_text", "Java", "Can't find search input", 5)
+        waitElementByXpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']//*[@text='Object-oriented programming language']",
+                "Can't find Object-oriented programming language", 10)
+    }
 
-        var enter: WebElement = waitElementById("search_src_text", "Can't find search input", 5)
-        enter.sendKeys("Madonna")
+    @Test
+    fun cancelSearchTest() {
+        clickByXpath("//*[contains(@text, 'Search Wikipedia')]", "Can't find Search Wikipedia input", 5)
+        closeSearch("search_close_btn", "Can't find close button", 5)
+        hasNotCloseButton("search_close_btn", "There's search button", 5)
     }
 
     fun waitElementById(id: String, error: String, time: Long): WebElement {
@@ -50,6 +55,62 @@ open class FirstTest {
         var by: By = By.id(id)
         return wait.until(
                 ExpectedConditions.presenceOfElementLocated(by)
+        )
+    }
+
+    fun waitElementById(id: String, error: String): WebElement {
+        return waitElementById(id, error, 5)
+    }
+
+    fun waitElementByXpath(path: String, error: String, time: Long): WebElement {
+        var wait = WebDriverWait(driver, time)
+        wait.withMessage(error + "\n")
+        var by: By = By.xpath(path)
+        return wait.until(
+                ExpectedConditions.presenceOfElementLocated(by)
+        )
+    }
+
+    fun waitElementByXpath(path: String, error: String): WebElement {
+        return waitElementById(path, error, 5)
+    }
+
+    fun clickByXpath(path: String, error: String, time: Long): WebElement {
+        val element: WebElement = waitElementByXpath(path, error, time)
+        element.click()
+        return element
+    }
+
+    fun clickById(id: String, error: String, time: Long): WebElement {
+        val element: WebElement = waitElementById(id, error, time)
+        element.click()
+        return element
+    }
+
+    fun sendKeysByXpath(path: String, text: String, error: String, time: Long): WebElement {
+        val element: WebElement = waitElementByXpath(path, error, time)
+        element.sendKeys(text)
+        return element
+    }
+
+    fun sendKeysById(id: String, text: String, error: String, time: Long): WebElement {
+        val element: WebElement = waitElementById(id, error, time)
+        element.sendKeys(text)
+        return element
+    }
+
+    fun closeSearch(id: String, error: String, time: Long): WebElement {
+        val element: WebElement = waitElementById(id, error, time)
+        element.click()
+        return element
+    }
+
+    fun hasNotCloseButton(id: String, error: String, time: Long): Boolean {
+        var wait = WebDriverWait(driver, time)
+        wait.withMessage(error + "\n")
+        var by: By = By.id(id)
+        return wait.until(
+                ExpectedConditions.invisibilityOfElementLocated(by)
         )
     }
 }
